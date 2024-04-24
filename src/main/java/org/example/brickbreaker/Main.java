522a85ac9859ff01664d15351116a6ea3dc96395
Mohamed Adel <mohamedadel96e@gmail.com>
2024-04-24 17:55:49 +0200

    Session 1

--- src/main/java/org/example/brickbreaker/Main.java
+++ src/main/java/org/example/brickbreaker/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Brick Breaker");
         stage.setScene(scene);
         stage.show();
     }
